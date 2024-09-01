6523501e9e6367120fed481cd0837fe2951a445b
Mohansai <bandarumohansai@gmail.com>
2024-09-01 17:44:23 +0530

    Simple ansible script

---
+++ 01-AnsibleServer_and_AnsibleNode_Connection.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ec2-user=$1
+
+echo "Checking that ansible node is connected and can be accessed from Ansible server or not"
+ansible -i ${ec2-user}, all -e ansible_user=ec2-user -e ansible_password=DevOps321 -m ping
+
+echo "Insatlling the nginx service"
+ansible -i ${ec2-user}, all -e ansible_user=ec2-user -e ansible_password=DevOps321 -b -m dnf -a "name=nginx state=installed"
+
+echo "Starting the nginx service"
+ansible -i ${ec2-user}, all -e ansible_user=ec2-user -e ansible_password=DevOps321 -b -m service -a "name=nginx state=started"
