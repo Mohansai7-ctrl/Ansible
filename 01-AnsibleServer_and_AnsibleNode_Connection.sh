26cd0f5e752e18c3059653abedf141897991bdde
Mohansai <bandarumohansai@gmail.com>
2024-09-01 17:47:26 +0530

    Simple ansible script

--- 01-AnsibleServer_and_AnsibleNode_Connection.sh
+++ 01-AnsibleServer_and_AnsibleNode_Connection.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ec2-user=$1
+ec2-user=$(ec2-user@$1)
 
 echo "Checking that ansible node is connected and can be accessed from Ansible server or not"
 ansible -i ${ec2-user}, all -e ansible_user=ec2-user -e ansible_password=DevOps321 -m ping
